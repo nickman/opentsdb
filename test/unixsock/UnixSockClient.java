fbc92b12d436362296b2977c84078eaee5d935c1
nickman <whitehead.nicholas@gmail.com>
2017-01-30 14:52:23 -0500

    DevSynch

---
+++ test/unixsock/UnixSockClient.java
@@ -0,0 +1,77 @@
+// This file is part of OpenTSDB.
+// Copyright (C) 2010-2012  The OpenTSDB Authors.
+//
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 2.1 of the License, or (at your
+// option) any later version.  This program is distributed in the hope that it
+// will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+// of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser
+// General Public License for more details.  You should have received a copy
+// of the GNU Lesser General Public License along with this program.  If not,
+// see <http://www.gnu.org/licenses/>.
+package net.opentsdb.unixsock;
+
+import io.netty.channel.*;
+import io.netty.channel.epoll.*;
+import io.netty.channel.unix.*;
+
+import java.io.File;
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+
+
+/**
+ * <p>Title: UnixSockClient</p>
+ * <p>Description: Unix socket client to test the unix socket server</p> 
+ * <p><code>net.opentsdb.unixsock.UnixSockClient</code></p>
+ */
+
+public class UnixSockClient {
+
+	public static final Charset UTF8 = Charset.forName("UTF8");
+	/**
+	 * Creates a new UnixSockClient
+	 */
+	public UnixSockClient() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static void log(final Object msg) {
+		System.out.println(msg);
+	}
+	
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Epoll.ensureAvailability();
+		log("Epoll available:" + Epoll.isAvailable());
+		final File file = new File("/tmp/opentsdb.sock");
+		final DomainSocketAddress address = new DomainSocketAddress(file);
+		log("DomainSocketAddress:" + address + "\n\tPort:");
+		Socket usock = null;
+		try {
+			usock = Socket.newSocketDomain();			
+			log("Created Socket:" + usock);
+			boolean connected = usock.connect(address);
+			log("Connected Socket:" + usock + ":" + connected);
+			connected = usock.finishConnect();
+			log("Connect finished:" + connected + ", Remote:" + usock.remoteAddress() + ", Port:" + usock.remoteAddress().getPort());
+			
+			final String put = "put sna.foo 1460070776 4 dc=dcX host=host7\n";
+			final byte[] bytes = put.getBytes(UTF8);
+			final ByteBuffer b = ByteBuffer.allocateDirect(bytes.length);
+			b.put(bytes);
+//			final int bytesWritten = usock.sendTo(b, 0, bytes.length, usock.remoteAddress().getAddress(), usock.remoteAddress().getPort());
+			final int bytesWritten = usock.write(b, 0, bytes.length);			
+			log("Wrote Bytes:" + bytesWritten);
+		} catch (Exception ex) {
+			ex.printStackTrace(System.err);
+		} finally {
+			try { usock.close(); } catch (Exception x) {/* No Op */}			
+		}
+	}
+
+}
